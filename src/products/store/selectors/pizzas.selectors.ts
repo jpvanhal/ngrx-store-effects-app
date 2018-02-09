9736a6f424873a54a11f96edd09d609cdde0fb53
Janne Vanhala <janne.vanhala@gmail.com>
2018-02-09 14:07:06 +0200

    Mapping IDs to Entities

--- src/products/store/selectors/pizzas.selectors.ts
+++ src/products/store/selectors/pizzas.selectors.ts
@@ -3,6 +3,8 @@ import { createSelector } from '@ngrx/store';
 import * as fromRoot from '../../../app/store';
 import * as fromFeature from '../reducers';
 import * as fromPizzas from '../reducers/pizzas.reducers';
+import * as fromToppings from './toppings.selectors';
+
 import { Pizza } from '../../models/pizza.model';
 
 export const getPizzaState = createSelector(
@@ -22,6 +24,16 @@ export const getSelectedPizza = createSelector(
     return router.state && entities[router.state.params.pizzaId];
   },
 );
+
+export const getPizzaVisualised = createSelector(
+  getSelectedPizza,
+  fromToppings.getToppingEntities,
+  fromToppings.getSelectedToppings,
+  (pizza, toppingEntities, selectedToppings) => {
+    const toppings = selectedToppings.map((id) => toppingEntities[id]);
+    return {...pizza, toppings };
+  }
+)
 
 export const getAllPizzas = createSelector(getPizzasEntities, (entities) => {
   return Object.keys(entities).map((id) => entities[parseInt(id, 10)]);
